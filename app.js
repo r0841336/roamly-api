f261408b14e3e2c32b5e949d483438aea49b1487
Dante <r0841336@student.thomasmore.be>
2025-01-18 15:24:56 +0100

    more spelling changes

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Using the routes
-app.use('/api/v1/trips', tripRouter);
+app.use('/api/v1/trips', tripRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
