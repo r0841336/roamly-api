6b14702b0a8edead7af306dd055e48c5a8e03e11
Dante <r0841336@student.thomasmore.be>
2025-04-24 14:25:00 +0200

    fix: get password resetcode

--- controllers/api/v1/Users.js
+++ controllers/api/v1/Users.js
@@ -155,7 +155,7 @@ const forgotPassword = async (req, res) => {
         }
 
         // Genereer een willekeurige resetcode
-        const resetPasswordCode = crypto.randomBytes(6).toString('hex');
+        const resetPasswordCode = crypto.randomBytes(3).toString('hex'); // Code van 6 tekens
         const resetPasswordExpire = Date.now() + 3600000; // Code vervalt na 1 uur
 
         // Sla de resetcode en vervaldatum op in de database
