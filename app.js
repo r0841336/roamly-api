6fcb3f85d3c82169b33ccb5eb2470ddfb660f4c5
Thibault Rondelez <thibault.rondelez@outlook.com>
2025-06-05 12:12:58 +0200

    refresh error attempt

--- app.js
+++ app.js
@@ -45,6 +45,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/v1/trips', tripRoutes); // Gebruik de routes voor trips
 app.use('/api/v1/users', userRoutes); // Gebruik de routes voor users (toegevoegd)
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
